9bd85d91395d65e5822097093e42da7f48610d16
AnnaOrlova <annasa91@mail.ru>
2023-02-11 16:35:14 +0500

    задание2

---
+++ HW4/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int length = n.Length;
+int sum = 0;
+for (int i = 0; i < length; i++)
+{
+    sum = Convert.ToInt32(n[i].ToString()) + sum;
+}
+
+Console.Write(sum);
