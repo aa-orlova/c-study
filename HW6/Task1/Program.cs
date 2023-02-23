c87f15013c8a577dddad920d9bdd62304f95a08e
AnnaOrlova <annasa91@mail.ru>
2023-02-23 20:52:04 +0500

    задание1

---
+++ HW6/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void ReleaseArray(int[] array)
+{
+  int count = 0;
+  foreach (int i in array)
+  {
+    if (i > 0)
+    count++;  
+  }
+  Console.WriteLine($"Положительных чисел: {count}");
+}
+
+Console.Clear();
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+ReleaseArray(array);
