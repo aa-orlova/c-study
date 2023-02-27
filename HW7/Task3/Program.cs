675e22f3a986ab1a91e669011ac14f08739ab427
AnnaOrlova <annasa91@mail.ru>
2023-02-27 18:31:12 +0500

    задание3

---
+++ HW7/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void SumColumns(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+            sum = sum + matrix[j, i];
+        Console.Write($"{sum} \t");
+    }
+}
+
+
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+SumColumns(matrix);
