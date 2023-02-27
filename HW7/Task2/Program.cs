ed4c126ca1ea10d1f60c2c22b6bc647b5ad8300b
AnnaOrlova <annasa91@mail.ru>
2023-02-27 18:14:45 +0500

    задание2

---
+++ HW7/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+Console.Clear();
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+
+Console.Write("Введите позицию элемента: ");
+int[] position = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+if(position[0] > matrix.GetLength(0) || position[1] > matrix.GetLength(1))
+    Console.Write("Такого элемента не существует");
+else
+{
+    Console.Write(matrix[position[0], position[1]]);
+}
+
